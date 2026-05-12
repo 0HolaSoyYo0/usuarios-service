682fda07131dea34f19f345e51692060fe482e1f
Benjamin <succinctsnail62@gmail.com>
2026-05-12 02:23:03 -0400

    Cambio de nombre de la tablas y columnas a las que se apuntan.

--- src/main/java/cl/hebraviva/usuarios_service/usuarios/entity/Usuario.java
+++ src/main/java/cl/hebraviva/usuarios_service/usuarios/entity/Usuario.java
@@ -12,26 +12,27 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "usuarios", schema = "schema_usuarios")
+@Table(name = "usuarios", schema = "usuarios_service")
 public class Usuario {
 
     @Id
+    @Column(name = "id_usuario")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
 
-    @Column(length = 30, nullable = false, name = "p_nombre")
+    @Column(length = 30, nullable = false, name = "nom_usuario")
     private String nombreUsuario;
 
-    @Column(length = 30, nullable = false, name = "p_apellido")
+    @Column(length = 30, nullable = false, name = "ap_usuario")
     private String apellidoUsuario;
 
-    @Column(length = 100, nullable = false, unique = true, name = "email")
+    @Column(length = 100, nullable = false, unique = true, name = "correo_usuario")
     private String emailUsuario;
 
-    @Column(length = 12, nullable = false, unique = true, name = "telefono")
+    @Column(length = 12, nullable = false, unique = true, name = "telefono_usuario")
     private String telefonoUsuario;
 
-    @Column(length = 16, nullable = false, unique = true, name = "contraseña")
+    @Column(length = 16, nullable = false, unique = true, name = "clave_usuario")
     private String passwordUsuario;
 
 }
